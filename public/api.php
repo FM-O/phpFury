075aaf1f557fd8f4a6a2612a640b922e52b0b7c3
thecrazyfox <flo69001@hotmail.fr>
2015-01-11 18:17:10 +0100

    API test

---
+++ public/api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: flo
+ * Date: 10/01/15
+ * Time: 16:20
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://connect.soundcloud.com/sdk.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+<a href="#" class="connect">Connect with SoundCloud</a>
+<p>Hello, your name is <span id="username"></span></p>
+<div id="avatar"></div>
+<div id="results"></div>
+</body>
+</html>
