1d13e4a619863cad9426ed833aa4e5748871b251
thecrazyfox <flo69001@hotmail.fr>
2015-01-13 08:43:02 +0100

    API test -VFinale

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 SC.initialize({
     client_id: '8ec40d909de5ff0c1b3da51095ed9902',
-    redirect_uri: 'http://phpfury.local/callback.html'
+    redirect_uri: 'http://florianmichel-online.com/API/callback'
+    //redirect_uri: 'http://phpfury.local/callback'
 });
 $(document).ready(function() {
     $('a.connect').click(function(e) {
@@ -18,13 +19,17 @@ $(document).ready(function() {
                     });
                 });
             });
+
             setTimeout(function(){
-                SC.get('/users/92795454/favorites', function(playlist){
-                    for(i=0; i<playlist.length; i++){
-                        console.log(playlist[i].title);
-                        $('#results').append($('<li></li>').html('Titre : '+playlist[i].title));
-                        $('#results').append($('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+playlist[i].id+'&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'));
-                    }
+                SC.get('/me', function(me){
+                    var id = me.id;
+                    SC.get('/users/'+id+'/favorites', function(playlist){
+                        for(i=0; i<playlist.length; i++){
+                            console.log(playlist[i].title);
+                            $('#results').append($('<li></li>').html('Titre : '+playlist[i].title));
+                            $('#results').append($('<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+playlist[i].id+'&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>'));
+                        }
+                    });
                 });
             }, 300);
         });
